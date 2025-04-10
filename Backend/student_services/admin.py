a436c0b093db6a51cae829f4f43fe439a26b539d
Yoseph Sagawa <yosephsagawa0@gmail.com>
2025-04-10 13:10:36 +0300

    Reinitialized the Backend

---
+++ Backend/student_services/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import RFIDTag,Student,Instructor,Classes,ClassEnrollment,Attendance,AccessControl,Dormitory,DormitoryAssignment,CafeteriaTransaction
+# Register your models here.
+admin.site.register(RFIDTag)
+admin.site.register(Student)
+admin.site.register(Instructor)
+admin.site.register(Classes)
+admin.site.register(ClassEnrollment)
+admin.site.register(Attendance)
+admin.site.register(AccessControl)
+admin.site.register(Dormitory)
+admin.site.register(DormitoryAssignment)
+admin.site.register(CafeteriaTransaction)
